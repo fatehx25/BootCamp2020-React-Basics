045db4bdf6f70f06ddca28e71329db65552c10e7
FGolani1 <fatehx25@gmail.com>
2020-06-12 18:16:04 +0500

    changes in src\App,js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Okay! This is my first React App. <b>Preety <strong>Easy</strong> Ahn!</b> 
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
