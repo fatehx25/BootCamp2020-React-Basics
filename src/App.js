7c4da72171fc0c3f57db1c047983f44a1f1f96b4
FGolani1 <fatehx25@gmail.com>
2020-06-12 18:23:28 +0500

    changed App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>FATEH ALI MEHDI GOLANI</code>
+          FATEH ALI MEHDI GOLANI
         <br/>
           Okay! This is my first React App. <b>Preety <strong>Easy</strong> Ahn!</b> 
         </p>
